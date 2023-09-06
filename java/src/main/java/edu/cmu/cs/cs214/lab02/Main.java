c2a6e5cfd9f5cb7f5b4da911023a38b831f2d055
Qiaowen Wu <qiaowenwu@qiaowens-mbp.wifi.local.cmu.edu>
2023-09-06 11:59:21 -0400

    improved encapsulation change interface

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,24 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
 
-        Renderer renderer = new Renderer(rectangle);
+        Shape circle = new Circle(5.0);
+        Shape square = new Square(3.0);
+        Shape rectangle = new Rectangle(2, 3);
 
-        renderer.draw();
+
+        Renderer circleRenderer = new Renderer(circle);
+        Renderer rectangleRenderer = new Renderer(rectangle);
+        Renderer squareRenderer = new Renderer(square);
+
+        circleRenderer.draw();
+        rectangleRenderer.draw();
+        squareRenderer.draw();
     }
 }
